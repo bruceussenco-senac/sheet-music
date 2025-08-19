f804dd9d83faa1d6ac6dce13d8ed741e696f2ed7
bruceussenco-senac <bruce.ussenco@alunos.sc.senac.br>
2025-08-19 19:56:26 -0300

    fix lines

--- main.py
+++ main.py
@@ -9,14 +9,27 @@ def clef_read():
     locations = np.where(result >= threshold)
 
     for pt in zip(*locations[::-1]):
-        cv2.rectangle(main_color, pt, (pt[0] + clef_template.shape[1], pt[1] + clef_template.shape[0]), (0, 0, 255), 2)
+        cv2.rectangle(main_color, pt, (pt[0] + clef_template.shape[1], pt[1] + clef_template.shape[0]-1), (0, 0, 255))
         clefs_heights.append(int(pt[1]))
 
+def note_read():
+    for template in note_templates:
+        tem_w = template.shape[1]
+        tem_h = template.shape[0]
+        template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        result = cv2.matchTemplate(main_image, template_gray, cv2.TM_CCOEFF_NORMED)
+        locations = np.where(result >= threshold)
 
+        for pt in zip(*locations[::-1]):
+            # Draw rectangles around detected areas
+            cv2.rectangle(main_color, pt, (pt[0] + tem_w, pt[1] + tem_h), (0, 255, 0))
+            note_positions.append((
+                int(pt[0]) + tem_w/2,
+                int(pt[1]) + tem_h/2
+            ))
+            note_heights.add(pt[1] + int(tem_h/2))
 
 n_count = 4
-
-clefs_heights = []
 
 # Load the main image and the template image
 main_color = cv2.imread('ode_to_joy.png', cv2.IMREAD_COLOR)
@@ -29,27 +42,29 @@ for i in range(n_count):
 
 note_positions = []
 note_heights = set()
+clefs_heights = []
 
 clef_read()
+note_read()
 
-for template in note_templates:
-    template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    result = cv2.matchTemplate(main_image, template_gray, cv2.TM_CCOEFF_NORMED)
-    locations = np.where(result >= threshold)
-
-    # Draw rectangles around detected areas
-    for pt in zip(*locations[::-1]):
-        cv2.rectangle(main_color, pt, (pt[0] + template.shape[1], pt[1] + template.shape[0]), (0, 255, 0), 2)
-        note_positions.append((int(pt[0]), int(pt[1])))
-        note_heights.add(pt[1])
 
 # Draw lines for heights
 for h in note_heights:
     cv2.line(main_color, (0, h), (main_color.shape[0], h), (255, 0, 0))
 
+# Line references with clefs positions
+print("--- clef heights: ---")
+for h in clefs_heights:
+    print(h)
+    ch = h + clef_template.shape[0] - 1
+    cv2.line(main_color, (500, h), (main_color.shape[0], h), (0, 0, 255))
+    cv2.line(main_color, (500, ch), (main_color.shape[0], ch), (0, 0, 255))
+print("------")
+
 # Display the result
 detected_image = cv2.resize(main_color, (int(main_color.shape[1]/2), int(main_color.shape[0]/2)), interpolation=cv2.INTER_AREA)
 cv2.imshow('Detected Image', detected_image)
+cv2.imwrite('export.png', main_color)
 
 for pos in note_positions:
     print(pos)
